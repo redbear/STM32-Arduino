eca7b5cd4120c77b747902255caedca664a94824
lvjianxing <jianxing@redbear.cc>
2016-03-11 14:57:20 +0800

    update the Duo RGB example

--- arduino/libraries/RedBear_Duo/examples/01.Basics/Duo_RGB/Duo_RGB.ino
+++ arduino/libraries/RedBear_Duo/examples/01.Basics/Duo_RGB/Duo_RGB.ino
@@ -1,32 +1,27 @@
 // -----------------------------------
 // Controlling the RGB LED 
 // -----------------------------------
-#include "application.h"
-#include "ChainableLED.h"
-
 #if defined(ARDUINO) 
 SYSTEM_MODE(MANUAL);//do not connect to cloud
 #else
 SYSTEM_MODE(AUTOMATIC);//connect to cloud
 #endif
 
-#define NUM_LEDS  1
-ChainableLED rgb(D4, D5, NUM_LEDS);
 
 // This routine runs only once upon reset
 void setup()
 {
-  rgb.setColorRGB(0, 0, 0, 0);                 // turn down the LED
+  RGB.control(true);
 }
 
 // This routine loops forever
 void loop()
 {
-    rgb.setColorRGB(0,50, 0, 0);                 // turn down the LED
+    RGB.color(255, 0, 0);                 // turn down the LED
     delay(500);
-    rgb.setColorRGB(0, 0 ,50,  0);                 // turn down the LED
+    RGB.color(0, 255 ,0);                 // turn down the LED
     delay(500);
-    rgb.setColorRGB(0, 0 , 0, 50);                 // turn down the LED
+    RGB.color(0, 0,255);                 // turn down the LED
     delay(500);
 }
 
