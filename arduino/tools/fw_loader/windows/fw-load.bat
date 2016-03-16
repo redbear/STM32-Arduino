3ca68ff18ebbda840065dcbfcf4135cf45a1d55a
lvjianxing <jianxing@redbear.cc>
2016-03-16 14:47:29 +0800

    update the fw-load.bat

--- arduino/tools/fw_loader/windows/fw-load.bat
+++ arduino/tools/fw_loader/windows/fw-load.bat
@@ -1,17 +1,19 @@
-@echo off
+@echo off   
 
-rem System firmware update script for the RedBear Duo 
 
-rem Update System-Part1    
+rem System firmware update script for the RedBear Duo   
 
-%1 -d 2b04:d058 -a 0 -s 0x08020000 -D %2/duo-system-part1.bin    
-
+rem Update System-Part1        
+  
+   %1 -d 2b04:d058 -a 0 -s 0x08020000 -D %2/duo-system-part1.bin       
+   
 rem Update System-Part2  
-
-%1 -d 2b04:d058 -a 0 -s 0x08040000 -D %2/duo-system-part2.bin     
-
-rem Update User-Part   %1 -d 2b04:d058 -a 0 -s 0x080C0000 -D %2/duo-user-part.bin    
-
-rem Update Factory Reset Part and exit DFU mode
-
-%1 -d 2b04:d058 -a 2 -s 0x140000:leave -D %2/duo-user-part.bin    
+   
+   %1 -d 2b04:d058 -a 0 -s 0x08040000 -D %2/duo-system-part2.bin       
+   
+   rem Update User-Part   
+         %1 -d 2b04:d058 -a 0 -s 0x080C0000 -D %2/duo-user-part.bin    
+     
+   rem Update Factory Reset Part and exit DFU mode
+   
+   %1 -d 2b04:d058 -a 2 -s 0x140000:leave -D %2/duo-user-part.bin        
