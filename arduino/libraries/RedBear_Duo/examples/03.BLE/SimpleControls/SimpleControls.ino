18c8e562f6969efd690c19d3abbbd56a7ca8e1ca
jixing.zou <jixing.zou@redbear.hk>
2016-03-23 15:03:52 +0800

    Add comments for example "SimpleControls".

--- arduino/libraries/RedBear_Duo/examples/03.BLE/SimpleControls/SimpleControls.ino
+++ arduino/libraries/RedBear_Duo/examples/03.BLE/SimpleControls/SimpleControls.ino
@@ -45,16 +45,28 @@ static uint8_t characteristic2_data[CHARACTERISTIC2_MAX_LEN]={0x00};
 static btstack_timer_source_t characteristic2;
 
 static advParams_t adv_params;
-static uint8_t adv_data[]={0x02,0x01,0x06,0x08,0x08,'B','i','s','c','u','i','t',0x11,0x07,0x1e,0x94,0x8d,0xf1,0x48,0x31,0x94,0xba,0x75,0x4c,0x3e,0x50,0x00,0x00,0x3d,0x71};
+static uint8_t adv_data[]={
+    0x02,0x01,0x06, // Flag.
+    0x08,0x08,'B','i','s','c','u','i','t', // Short local name.
+    0x11,0x07,0x1e,0x94,0x8d,0xf1,0x48,0x31,0x94,0xba,0x75,0x4c,0x3e,0x50,0x00,0x00,0x3d,0x71 // 128-bits complete UUID.
+};
 
+// Mark whether need to notify analog value to client.
 static boolean analog_enabled = false;
+// Input pin state.
 static byte old_state         = LOW;
 
- /******************************************************
+/******************************************************
  *               Function Definitions
  ******************************************************/
-
- 
+/**
+ * @brief Connect handle.
+ *
+ * @param[in]  status   BLE_STATUS_CONNECTION_ERROR or BLE_STATUS_OK.
+ * @param[in]  handle   Connect handle.
+ *
+ * @retval None
+ */
 void deviceConnectedCallback(BLEStatus_t status, uint16_t handle) {
     switch (status){
         case BLE_STATUS_OK:
@@ -65,10 +77,26 @@ void deviceConnectedCallback(BLEStatus_t status, uint16_t handle) {
     }
 }
 
+/**
+ * @brief Disconnect handle.
+ *
+ * @param[in]  handle   Connect handle.
+ *
+ * @retval None
+ */
 void deviceDisconnectedCallback(uint16_t handle){
     Serial.println("Disconnected.");
 }
 
+/**
+ * @brief Callback for writting event.
+ *
+ * @param[in]  value_handle  
+ * @param[in]  *buffer       The buffer pointer of writting data.
+ * @param[in]  size          The length of writting data.   
+ *
+ * @retval 
+ */
 int gattWriteCallback(uint16_t value_handle, uint8_t *buffer, uint16_t size)
 {
     Serial.print("Write value handler: ");
@@ -107,7 +135,7 @@ int gattWriteCallback(uint16_t value_handle, uint8_t *buffer, uint16_t size)
         {
             myservo.write(characteristic1_data[1]);
         }
-        else if (characteristic1_data[0] == 0x04)
+        else if (characteristic1_data[0] == 0x04)  // Command is to initialize all.
         {
             analog_enabled = false;
             myservo.write(0);
@@ -119,9 +147,16 @@ int gattWriteCallback(uint16_t value_handle, uint8_t *buffer, uint16_t size)
     return 0;
 }
 
+/**
+ * @brief Timer task for sending status change to client.
+ *
+ * @param[in]  *ts   
+ *
+ * @retval None
+ */
 static void  characteristic2_notify(btstack_timer_source_t *ts)
 {
-    if (analog_enabled)  // if analog reading enabled
+    if (analog_enabled)  // if analog reading enabled.
     {
         //Serial.println("characteristic2_notify analog reading ");
         // Read and send out
@@ -132,7 +167,7 @@ static void  characteristic2_notify(btstack_timer_source_t *ts)
         if(ble.attServerCanSendPacket())
             ble.sendNotify(character2_handle, characteristic2_data, CHARACTERISTIC2_MAX_LEN);
     }
-    // If digital in changes, report the state
+    // If digital in changes, report the state.
     if (digitalRead(DIGITAL_IN_PIN) != old_state)
     {
         Serial.println("characteristic2_notify digital reading ");
@@ -152,11 +187,14 @@ static void  characteristic2_notify(btstack_timer_source_t *ts)
             ble.sendNotify(character2_handle, characteristic2_data, CHARACTERISTIC2_MAX_LEN);
         }
     }
-    // reset
+    // Restart timer.
     ble.setTimer(ts, 200);
     ble.addTimer(ts);
 }
 
+/**
+ * @brief Setup.
+ */
 void setup()
 {
     Serial.begin(115200);
@@ -180,6 +218,14 @@ void setup()
     character1_handle = ble.addCharacteristicDynamic(service1_tx_uuid, ATT_PROPERTY_NOTIFY|ATT_PROPERTY_WRITE|ATT_PROPERTY_WRITE_WITHOUT_RESPONSE, characteristic1_data, CHARACTERISTIC1_MAX_LEN);
     character2_handle = ble.addCharacteristicDynamic(service1_rx_uuid, ATT_PROPERTY_NOTIFY, characteristic2_data, CHARACTERISTIC2_MAX_LEN);
     
+    // Set advertise parameters.
+    // @note  advertising_interval_min ([0x0020,0x4000], default: 0x0800, unit: 0.625 msec)
+    //        advertising_interval_max ([0x0020,0x4000], default: 0x0800, unit: 0.625 msec)
+    //        advertising_type (enum from 0: ADV_IND, ADC_DIRECT_IND, ADV_SCAN_IND, ADV_NONCONN_IND)
+    //        own_address_type (enum from 0: public device address, random device address)
+    //        advertising_channel_map (flags: reserved(0x00),chan_37(0x01), chan_38(0x02), chan_39(0x04), default(0x07,all channels enable))
+    // @note  If the advertising_type is set to ADV_SCAN_IND or ADV_NONCONN_IND,advertising_interval_min and advertising_interval_max shal not be set to less than 0x00A0.
+    //    
     adv_params.adv_int_min = 0x00A0;
     adv_params.adv_int_max = 0x01A0;
     adv_params.adv_type    = 0;
@@ -194,6 +240,7 @@ void setup()
 
     ble.startAdvertising();
 
+    // Initialize all peripheral.
     pinMode(DIGITAL_OUT_PIN, OUTPUT);
     pinMode(DIGITAL_IN_PIN, INPUT_PULLUP);
     pinMode(PWM_PIN, OUTPUT);
@@ -204,7 +251,7 @@ void setup()
     myservo.attach(SERVO_PIN);
     myservo.write(0);
 
-    // set one-shot timer
+    // Start a task to check status.
     characteristic2.process = &characteristic2_notify;
     ble.setTimer(&characteristic2, 500);//2000ms
     ble.addTimer(&characteristic2);
@@ -212,6 +259,9 @@ void setup()
     Serial.println("BLE start advertising.");
 }
 
+/**
+ * @brief Loop.
+ */
 void loop()
 {
     
