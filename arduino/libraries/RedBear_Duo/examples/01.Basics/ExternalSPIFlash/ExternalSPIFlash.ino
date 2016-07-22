35cc644e7a9bf5ea074ca013d3896750e59513bf
XuGuohui <guohui@redbear.cc>
2016-07-22 15:04:10 +0800

    Add example for demonstrating theusage of the external SPI flash

---
+++ arduino/libraries/RedBear_Duo/examples/01.Basics/ExternalSPIFlash/ExternalSPIFlash.ino
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2016 RedBear
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.
+ */ 
+
+/*
+ * SYSTEM_MODE:
+ *     - AUTOMATIC: Automatically try to connect to Wi-Fi and the Particle Cloud and handle the cloud messages.
+ *     - SEMI_AUTOMATIC: Manually connect to Wi-Fi and the Particle Cloud, but automatically handle the cloud messages.
+ *     - MANUAL: Manually connect to Wi-Fi and the Particle Cloud and handle the cloud messages.
+ *     
+ * SYSTEM_MODE(AUTOMATIC) does not need to be called, because it is the default state. 
+ * However the user can invoke this method to make the mode explicit.
+ * Learn more about system modes: https://docs.particle.io/reference/firmware/photon/#system-modes .
+ */
+#if defined(ARDUINO) 
+SYSTEM_MODE(SEMI_AUTOMATIC); 
+#endif
+
+uint8_t write_buf[] = {0x55, 0xAA, 0x5A, 0xA5, 0x6C, 0xC6};
+uint8_t read_buf[6];
+
+void setup() {
+  Serial.begin(115200);
+  delay(5000);
+
+  if ( sFLASH.selfTest() == 0 ) {
+    Serial.println("The external SPI flash functions well.");
+
+    Serial.println("Erase sector 0.");
+    sFLASH.eraseSector(0x0);
+
+    Serial.print("Store data to external SPI flash from address 0: ");
+    for (uint8_t i = 0; i < sizeof(write_buf); i++) {
+      Serial.print(write_buf[i], HEX);
+      Serial.print(", ");
+    }
+    Serial.println("");
+    sFLASH.writeBuffer(write_buf, 0, sizeof(write_buf));
+
+    // Read data from address 0.
+    sFLASH.readBuffer(read_buf, 0, sizeof(read_buf));
+    Serial.print("Read data from external SPI flash from address 0: ");
+    for (uint8_t i = 0; i < sizeof(read_buf); i++) {
+      Serial.print(read_buf[i], HEX);
+      Serial.print(", ");
+    }
+    Serial.println("");
+  }
+  else {
+    Serial.print("There is something wrong with the external SPI flash!");
+  }
+}
+
+void loop() {
+  // Do nothing.
+}
+
